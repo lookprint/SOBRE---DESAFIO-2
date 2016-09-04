57f38be518f1e6acf5ebef378dc385fcf92131b2
Patrick Magalhães <pixelzip0@gmail.com>
2016-09-04 12:59:33 -0300

    First commit

---
+++ js/master.js
@@ -0,0 +1,34 @@
+var spot = document.getElementById('spot');
+var width = document.documentElement.clientWidth;
+var height = document.documentElement.clientHeight;
+
+ 
+ 
+function moveSpot(e){
+    var x = 0;
+    var y = 0;
+    if (!e) var e = window.event;
+    if (e.pageX || e.pageY){
+        x = e.pageX;
+        y = e.pageY;
+    }
+    else if (e.clientX || e.clientY){
+        x = e.clientX + document.body.scrollLeft;
+        y = e.clientY + document.body.scrollTop;
+    }
+   
+    if (navigator.userAgent.match('AppleWebKit')) {
+        var style = '-webkit-gradient(radial, '+x+' '+y+', 0, '+x+' '+y+', 300, from(rgba(0,0,0,0)), to(rgba(0,0,0,0.8)), color-stop(0.8, rgba(0,0,0,0)))';
+    } else {
+        var style = '-moz-radial-gradient('+x+'px '+y+'px 45deg,circle closest-side,transparent 0,transparent 100px,rgba(0, 0, 0, 0.8) 120px)';
+    }
+    spot.style.backgroundImage = style;	
+}
+ 
+window.onload = function() {
+    window.onmousemove = moveSpot;
+
+}
+
+
+
